5f08983ee74ad6eead231959d251222f8ca55cc0
Dut <dutandrew78@gmail.com>
2023-06-11 20:53:55 +0300

    added polygon file

---
+++ inheritance/polygon.py
@@ -0,0 +1,20 @@
+class Polygon:
+    __width = None
+    __height = None
+
+    def get_width(self):
+        return self.__width
+    
+    def set_width(self, new_width):
+        self.__width = new_width
+
+    def get_height(self):
+        return self.__height
+    
+    def set_height(self, new_height):
+        self.__height = new_height
+
+rect = Polygon()
+rect.set_width(20)
+
+print(rect.get_width())
