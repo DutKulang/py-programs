81dfc35266ecf2fe9bfa671c116d4f69702c6dd4
Dut <dutandrew78@gmail.com>
2023-06-11 21:58:45 +0300

    added triangle

--- inheritance/triangle.py
+++ inheritance/triangle.py
@@ -1,7 +1,11 @@
 # importing the Polygon class from polygon.py file
 from polygon import Polygon
+class Triangle(Polygon):
+    def area(self):
+        return (self.get_height() * self.get_width()) / 2
 
-tria = Polygon()
-tria.set_width(20)
+tria = Triangle()
+tria.set_width(50)
+tria.set_height(25)
 
-print(tria.get_width())
+print(tria.area())
